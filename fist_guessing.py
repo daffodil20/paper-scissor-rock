7b1f33399099bd6f018f6b00c9dea9495642fdff
luxzhuo <luxzhuo@126.com>
2023-07-12 11:16:28 +0800

    there is some error after I use the case statement,perhaps the match statement has some problems

--- fist_guessing.py
+++ fist_guessing.py
@@ -11,119 +11,169 @@ while True:
     userA = input()
     option = ['p','s','r']
     userB = random.choice(option)
+    # num = input()
     # print('Enter the times of guessing(1/3/5):')
     # num = input()
     # a = 0
     # x = 0
     # y = 0
 #judge the condition
-    if userA =="p" and userB == "s":
-        a+=1
-        if num == 1:
-            print('userB wins')
+    start=input("The game begins!")
+    match start:
+        
+        case "userA =='p' and userB == 's' and num == 1":
+            # y+=1
+            # a+=1
+            # case "num == 1":
+            print('computer wins')
             False
-        else:
+    # if userA =="p" and userB == "s":
+    #     a+=1
+        case "userA =='p' and userB == 's' and num == 3":
+            a+=1
             y+=1
-            if num == 3 and a<3:
+            if a<3:
                 True
-            if num == 5 and a<5:
-                True
-            else:
-                if y>x:
-                    print('computer wins')
-                if y<x:
-                    print('userA wins')
+            if a==3 and x<y:
+                print("computer wins")
                 False
-    if userA =="s" and userB == "p":
-        a+=1
-        if num == 1:
+            if a==3 and x>y:
+                print("userA wins")
+                False
+        case "userA =='p' and userB == 's' and num == 5":
+            a+=1
+            y+=1
+            if a==5 and x<y:
+                print("computer wins")
+                False
+            if a==5 and x>y:
+                print("userA wins")
+                False
+       
+        case "userA =='s' and userB == 'p' and num==1":
             print('userA wins')
             False
-        else:
+        case "userA =='s' and userB == 'p' and num==3":
+            a+=1
             x+=1
-            if num == 3 and a<3:
+            if a<3:
                 True
-            if num == 5 and a<5:
-                True
-            else:
-                if y>x:
-                    print('computer wins')
-                if y<x:
-                    print('userA wins')
+            if a==3 and x<y:
+                print("computer wins")
+                False
+            if a==3 and x>y:
+                print("userA wins")
+                False
+        case "userA =='s' and userB == 'p' and num == 5":
+            a+=1
+            y+=1
+            if a==5 and x<y:
+                print("computer wins")
+                False
+            if a==5 and x>y:
+                print("userA wins")
                 False
 
-    if userA =="p" and userB == "r":
-        a+=1
-        if num == 1:
+        case "userA == 'p' and userB == 'r' and num == 1":
             print('userA wins')
             False
-        else:
+        case "userA =='p' and userB == 'r' and num == 3":
+            a+=1
             x+=1
-            if num == 3 and a<3:
+            if a<3:
                 True
-            if num == 5 and a<5:
-                True
+            if a==3 and x<y:
+                print("computer wins")
             else:
-                if y>x:
-                    print('computer wins')
-                if y<x:
-                    print('userA wins')
+                print("userA wins")
                 False
-    if userA =="r" and userB == "p":
-        a+=1
-        if num == 1:
-            print('userB wins')
-            False
-        else:
-            y+=1
-            if num == 3 and a<3:
-                True
-            if num == 5 and a<5:
-                True
-            else:
-                if y>x:
-                    print('computer wins')
-                if y<x:
-                    print('userA wins')
-                False
-
-    if userA =="s" and userB == "r":
-        a+=1
-        if num == 1:
-            print('userB wins')
-            False
-        else:
-            y+=1
-            if num == 3 and a<3:
-                True
-            if num == 5 and a<5:
-                True
-            else:
-                if y>x:
-                    print('computer wins')
-                if y<x:
-                    print('userA wins')
-                False
-
-    if userA =="r" and userB == "s":
-        a+=1
-        if num == 1:
-            print('userA wins')
-            False
-        else:
+        case "userA =='p' and userB == 'r' and num == 5":
+            a+=1
             x+=1
-            if num == 3 and a<3:
+            if a<5:
                 True
-            if num == 5 and a<5:
-                True
-            else:
-                if y>x:
-                    print('computer wins')
-                if y<x:
-                    print('userA wins')
+            if a==5 and x<y:
+                print("computer wins")
                 False
-
-    if userA == userB:
-        print('draw: no one wins')
-        True 
+            if a==5 and x>y:
+                print("userA wins")
+                False
+        case "userA =='r' and userB == 'p' and num == 1":
+            print("computer wins")
+            False
+        case "userA =='r' and userB == 'p' and num == 3":
+            a+=1
+            y+=1
+            if a<3:
+                True
+            if a==3 and x<y:
+                print("computer wins")
+                False
+            if a==3 and x>y:
+                print("userA wins")
+                False
+        case "userA =='r' and userB == 'p' and num == 5":
+            a+=1
+            y+=1
+            if a<5:
+                True
+            if a==5 and x<y:
+                print("computer wins")
+                False
+            if a==5 and x>y:
+                print("userA wins")
+                False
+        case "userA =='s' and userB == 'r' and num == 1":
+            print("computer wins")
+            False
+        case "userA =='s' and userB == 'r' and num == 3":
+            a+=1
+            y+=1
+            if a<3:
+                True
+            if a==3 and x<y:
+                print("computer wins")
+                False
+            if a==3 and x>y:
+                print("userA wins")
+                False
+        case "userA =='s' and userB == 'r' and num == 5":
+            a+=1
+            y+=1
+            if a<5:
+                True
+            if a==5 and x<y:
+                print("computer wins")
+                False
+            if a==5 and x>y:
+                print("userA wins")
+                False
+        case "userA =='r' and userB == 's' and num == 1":
+            print("userA wins")
+            False
+        case "userA =='r' and userB == 's' and num == 3":
+            a+=1
+            x+=1
+            if a<3:
+                True
+            if a==3 and x<y:
+                print("computer wins")
+                False
+            if a==3 and x>y:
+                print("userA wins")
+                False
+        case "userA =='r' and userB == 's' and num == 5":
+            a+=1
+            x+=1
+            if a<5:
+                True
+            if a==5 and x<y:
+                print("computer wins")
+                False
+            if a==5 and x>y:
+                print("userA wins")
+                False
+        case "userA == userB":
+            print('draw: no one wins')
+            True 
 
